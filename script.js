8c090229cac9c6abd352cfbbbe2fa7c73f017ff4
Pankaj-Sharmaa <147194794+Pankaj-Sharmaa@users.noreply.github.com>
2023-10-20 10:57:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-//your JS code here. If required.
+document.addEventListener("DOMContentLoaded", function () {
+    const squares = document.querySelectorAll(".square");
+
+    squares.forEach((square) => {
+        square.addEventListener("mouseenter", () => {
+            square.style.backgroundColor = "#3498db"; // Change the background color on hover
+        });
+
+        square.addEventListener("mouseleave", () => {
+            // Revert the background color after 1 second
+            setTimeout(() => {
+                square.style.backgroundColor = "#fff";
+            }, 1000);
+        });
+    });
+});
